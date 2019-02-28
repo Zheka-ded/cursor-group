f280128032b4e4c194a1171063ce405749c962b5
Zheka-ded <11vitamin11@rambler.ru>
2019-02-28 23:13:24 +0200

    28/02/19(added folder hw5StartJS)

---
+++ hw5StartJS/js/variables.js
@@ -0,0 +1,39 @@
+// alert('Hello iOld');
+
+// let message;
+// message = `Hello`;
+// alert(message);
+
+// let message = `Hello!`;
+// alert(message);
+
+// let user = iOld, age = 33, message = `hello iOld`;
+// !!! так как выше можно но не рекомендуется !!! //
+// !!! плохая читабельность кода !!! //
+
+// let user = iOld;
+// let age = 33;
+// let message = `Hello iOld`;
+
+// let message = `Hello`;
+//     message = `World`;
+// alert(message);
+
+// let hello = `Hello World`;
+// let message;
+// message = hello;
+// alert(hello);
+// alert(message);
+
+// Задачи
+// Работа с переменными
+// важность: 2
+// Объявите две переменные: admin и name.
+// Запишите в name строку "Василий".
+// Скопируйте значение из name в admin.
+// Выведите admin (должно вывести «Василий»).
+
+let name = `iOld`;
+let admin;
+admin = name;
+alert(admin);
