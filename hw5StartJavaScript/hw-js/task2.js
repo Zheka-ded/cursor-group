68a7a37bbaf04038b48e4225c1ea56dae2bfb49c
Zheka-ded <11vitamin11@rambler.ru>
2019-03-08 00:03:24 +0200

    07/03/19(without changes)

--- hw5StartJavaScript/hw-js/task2.js
+++ hw5StartJavaScript/hw-js/task2.js
@@ -1,34 +1,16 @@
-// A function which returns true if string contains ironman or cursor or ostap otherwise returns false. The function must be case-insensitive. //
+// function reverse(str) { 
+    
+//     return str.split("").reverse().join("");
 
-// function cursorCheck(str) { /* your code */ }
-// cursorCheck('Hello I am OstaP') // true
-// cursorCheck('Superman is here') // false
+//     // let str = 'abc';
 
-// // рабочий но чуть не то что нужно //
+//     // str = str.split("").reverse().join("");
+// }
+// reverse('abc') // 'cba'
+// // console.log(reverse(str));
 
-// let who = prompt(`Who?`, ``);
-
-// if (who === `ironman` || who === `cursor` || who === `ostap`) {
-//     alert('Hello I am OstaP'); // true
-// }   else{
-//         alert('Superman is here'); // false
-//     }
-
-// не знаю который код правильный, оба рабочие //
-
-// это тоже рабочий и он тоже без игнора регистра //
-
-function cursorCheck(str) { 
-    if (str === `ironman` || str === `cursor` || str === `ostap`) {
-        return true;
-    }   else {
-        return alert('Superman is here');
-    }
- }
-
- let str = prompt(`Who?`);
-
- if (cursorCheck(str)) {
-    alert('Hello I am OstaP');
- }
-
+// var str = "→Привет, Мир!";
+// var strRev = ""; // строка в обратном порядке
+// for (i = str.length-1; i >= 0; i--) {
+//   strRev += str[i];
+// }
---
+++ hw5StartJavaScript/hw-js/task8.js
@@ -0,0 +1,62 @@
+// A function which returns true if string contains ironman or cursor or ostap otherwise returns false. The function must be case-insensitive. //
+
+// function cursorCheck(str) { /* your code */ }
+// cursorCheck('Hello I am OstaP') // true
+// cursorCheck('Superman is here') // false
+
+// // рабочий но чуть не то что нужно //
+
+// let who = prompt(`Who?`, ``);
+
+// if (who === `ironman` || who === `cursor` || who === `ostap`) {
+//     alert('Hello I am OstaP'); // true
+// }   else{
+//         alert('Superman is here'); // false
+//     }
+
+// не знаю который код правильный, оба рабочие //
+
+// это тоже рабочий и он тоже без игнора регистра //
+
+// function cursorCheck(str) { 
+//     if (str === `ironman` || str === `cursor` || str === `ostap`) {
+//         return true;
+//     }   else {
+//         return alert('Superman is here');
+//     }
+//  }
+
+//  let str = prompt(`Who?`);
+
+//  if (cursorCheck(str)) {
+//     alert('Hello I am OstaP');
+//  }
+
+
+//  console.log(cursorCheck('Hello I am OstaP'));
+//  console.log(cursorCheck('Superman is here'));
+
+
+
+// cursorCheck('Hello I am OstaP') // true
+// cursorCheck('Superman is here') // false
+
+// let str = [`ironman`, `cursor`, `ostap`];
+
+ function cursorCheck(str) {
+    if (str === `ironman`) {
+        return true;
+    } else if (str === `cursor`) {
+        return true;
+    } else if (str === `ostap`) {
+        return true;
+    } else{
+        return false;
+    }
+ }
+
+//  let str = prompt(`Who?`);
+
+//  if (cursorCheck(str)) {
+//     alert('Hello I am OstaP');
+//  }
