7324a63732795424fc8a2dea3f91b4c5a3ef7808
Zheka-ded <11vitamin11@rambler.ru>
2019-03-21 20:22:53 +0200

    21/03/19(finish js hw 1)

--- hw5StartJavaScript/js-hw-1/task11.js
+++ hw5StartJavaScript/js-hw-1/task11.js
@@ -1,22 +1,22 @@
 // A function that when given a number n returns the n-th number in the Fibonacci Sequence. The sequence starts from 1 and looks like this: 1, 1, 2, 3, 5, 8, 13, 21… //
 // Функция, которая при задании числа nвозвращает n-е число в последовательности Фибоначчи . Последовательность начинается с 1 и выглядит так: 1, 1, 2, 3, 5, 8, 13, 21… //
 
-// function fibonacci(n) { /* your code */ }
-// fibonacci(3) // 2
-// fibonacci(5) // 5
-// fibonacci(7) // 13
+// Ниже ссылка на решение этой задачи //
+// https://learn.javascript.ru/task/fibonacci-numbers //
 
 function fibonacci(n) {
-    let result = 0;
-
-    for (let i = 1; i <= n; i++) {
-        result = n-1;
-        console.log(result, n, i);
+    let a = 1,
+        b = 1;
+    for (let i = 3; i <= n; i++) {
+        let c = a + b;
+        a = b;
+        b = c;
+        
     }
-
-    // return result;
-    // console.log(result);
+    return b;
 }
-fibonacci(3) // 2
-// fibonacci(5) // 5
-// fibonacci(7) // 13
+
+console.log(fibonacci(3));
+console.log(fibonacci(5));
+console.log(fibonacci(7));
+console.log(fibonacci(90));
