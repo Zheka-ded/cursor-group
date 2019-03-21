8a3075e7f8370c8c74d61211f40740d994925e3c
Zheka-ded <11vitamin11@rambler.ru>
2019-03-21 22:32:04 +0200

    21/03/19(start hw-js-2, done task 7)

---
+++ hw6StartJavaScript/js-hw-2/task1.js
@@ -0,0 +1,5 @@
+// function factorial(n) {
+    
+// }
+// // factorial(3); // 6
+// // factorial(5); // 120
---
+++ hw6StartJavaScript/js-hw-2/task7.js
@@ -0,0 +1,55 @@
+// // Функция, которая возвращает объект с минимальными и максимальными числами из массива чисел. //
+
+
+function minMax(arr) {
+    let min = arr[0];
+    let max = arr[0];
+
+    for (let i = 0; i < arr.length; i++){
+        if ( min > arr[i]) min = arr[i];
+        if(max < arr[i]) max = arr[i];
+    }
+
+    return{
+        min,
+        max
+    }
+
+    // return[
+    //     min,
+    //     max
+    // ]
+}
+console.log(minMax([1, 4, 8, 2, -20, (23 - 23445), 9432, 4, -3456, -9477]));
+
+
+// function min(arr) {
+//     let min = arr[0];
+//     // в min будет сщхранено минимальное значение массива //
+
+//     for (let i = 0; i < arr.length; i++) {
+//         // цикл работает по всей длинне массива //
+
+//         if ( min > arr[i]) min = arr[i];
+//         // если минимальное значение больше значения массива 
+//         // минимальное значение перезаписывается, и так по 
+//         // всей длинне массива
+
+//     }
+//     return min;
+// }
+// // min([1, 4, 8, 2, 20]) // { max: 20, min: 1 }
+// console.log(min([1, 4, 8, 2, -20, 432, 4, -3456, -477]));
+// console.log(min([1, 4, 8, 2, -20, (23 - 23445), 432, 4, -3456, -477]));
+
+// function max(arr) {
+//     let max = arr[0];
+
+//     for (let i = 0; i < arr.length; i++) {
+//         if(max < arr[i]) max = arr[i];
+//     }
+
+//     return max;
+// }
+// // max([1, 4, 8, 2, 20]) // { max: 20, min: 1 }
+// console.log(max([1, 4, 8, 2, -20, 432, 4, 3456, -477]));
