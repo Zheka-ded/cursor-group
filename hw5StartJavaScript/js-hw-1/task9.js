2c1975307bc445923fbd2ec83998b99408a959b0
Zheka-ded <11vitamin11@rambler.ru>
2019-03-26 22:11:01 +0200

    26/03/19(task-9 hw-1-js rework)

--- hw5StartJavaScript/js-hw-1/task9.js
+++ hw5StartJavaScript/js-hw-1/task9.js
@@ -1,5 +1,20 @@
 // Функция, которая возвращает строку со всеми буквами в верхнем регистре (без встроенного метода строки). Функция поддерживает только английский алфавит.//
 
+const uppStr2 = (str) => {
+  return str.split('').map.call(str, (el, i) => {
+ const charCode = el.charCodeAt(0);
+ 
+ const isLowerCaseChar = charCode >= 97 && charCode <= 122;
+ if(isLowerCaseChar) {
+   return String.fromCharCode(charCode - 32);
+ } 
+ return el;
+}).join('');
+}
+console.log(uppStr2(`dfghjhgf ну да ну да`));
+
+
+
 function uppStr(str) {
     for (let i = 0; i <= str.length; i++) {
         return str.toUpperCase();
@@ -7,15 +22,16 @@ function uppStr(str) {
 }
 console.log(uppStr(`asdrfhvj`));
 
-// переделать //
+// // переделать //
 
-const toUpperCase = str =>
-  Array.prototype.map.call(str, (el, i) => {
-   const charCode = el.charCodeAt(0);
+// const toUpperCase = str =>
+//   Array.prototype.map.call(str, (el, i) => {
+//    const charCode = el.charCodeAt(0);
 
-   const isLowerCaseChar = charCode >= 97 && charCode <= 122;
-   if(isLowerCaseChar) {
-     return String.fromCharCode(charCode - 32);
-   } 
-   return el;
-  }).join('');
+//    const isLowerCaseChar = charCode >= 97 && charCode <= 122;
+//    if(isLowerCaseChar) {
+//      return String.fromCharCode(charCode - 32);
+//    } 
+//    return el;
+//   }).join('');
+
